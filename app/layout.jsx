6f51397a2f340ed2c371a4faff573a20ec7b1e3c
deepyadav01 <chandradeepyadav2@gmail.com>
2024-11-18 15:13:39 +0530

    added ms clarity

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 // import ClarityScript from "@/components/ClarityScript"
+import Script from "next/script";
 
 
 const jetBrainsMono = JetBrains_Mono({
@@ -28,6 +29,15 @@ export default function RootLayout({ children }) {
         <Header />
         <StairTransition/>
         <PageTransition>{children}</PageTransition>
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ozjgblsi0g");
+          `}
+        </Script>
       </body>
     </html>
   );
