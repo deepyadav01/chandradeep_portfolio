756e9d56f76608b33c6bdc8b113ee56f408b6294
deepyadav01 <chandradeepyadav2@gmail.com>
2024-11-18 15:17:13 +0530

    added ms clarity

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,6 @@ import StairTransition from "@/components/StairTransition";
 // import ClarityScript from "@/components/ClarityScript"
 import Script from "next/script";
 
-
 const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -21,20 +20,19 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <body className={jetBrainsMono.variable}>
         {/* <ClarityScript/> */}
         <Header />
-        <StairTransition/>
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
-        <Script id="clarity" strategy="afterInteractive">
+        <Script id="clarity" strategy="afterInteractive" type="text/javascript">
           {`
             (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "ozjgblsi0g");
           `}
         </Script>
